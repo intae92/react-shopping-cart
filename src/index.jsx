5ec63c73c614ebc93f221571f0066a9f44895a8f
intae92 <kangit2013@gmail.com>
2022-05-28 15:20:58 +0900

    fix: msw url 수정

--- src/index.jsx
+++ src/index.jsx
@@ -8,25 +8,33 @@ import store from './store';
 import { ThemeProvider } from 'styled-components';
 import theme from './styles/theme';
 
-if (process.env.NODE_ENV === 'development') {
-  const { worker } = require('./mocks/worker');
+async function main() {
+  if (process.env.NODE_ENV === 'development') {
+    if (window.location.pathname === '/react-shopping-cart') {
+      window.location.pathname = '/react-shopping-cart/';
+      return;
+    }
+    const { worker } = require('./mocks/worker');
 
-  worker.start({
-    serviceWorker: {
-      url: `/react-shopping-cart/mockServiceWorker.js`,
-    },
-  });
+    await worker.start({
+      serviceWorker: {
+        url: `/react-shopping-cart/mockServiceWorker.js`,
+      },
+    });
+  }
+
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+    <React.StrictMode>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
+          <Provider store={store}>
+            <App />
+          </Provider>
+        </BrowserRouter>
+      </ThemeProvider>
+    </React.StrictMode>
+  );
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
-        <Provider store={store}>
-          <App />
-        </Provider>
-      </BrowserRouter>
-    </ThemeProvider>
-  </React.StrictMode>
-);
+main();
